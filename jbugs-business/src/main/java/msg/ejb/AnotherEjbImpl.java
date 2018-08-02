d7abf10a70178ddf0aa130040b0e9dfa0eb74ddd
ioangrozea <ioan.grozea@gmail.com>
2018-08-02 09:14:52 +0300

    pom config

--- jbugs-business/src/main/java/msg/ejb/AnotherEjbImpl.java
+++ jbugs-business/src/main/java/msg/ejb/AnotherEjbImpl.java
@@ -4,7 +4,6 @@ import javax.ejb.Stateless;
 
 @Stateless
 public class AnotherEjbImpl implements AnotherEjb {
-
     @Override
     public void doSomething() {
         System.out.println("do nothing");

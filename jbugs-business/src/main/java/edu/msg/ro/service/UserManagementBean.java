739d804645047c9d0c141ad672f29593c394b0ee
ioangrozea <ioan.grozea@gmail.com>
2018-08-02 15:35:32 +0300

    added business user logic

--- jbugs-business/src/main/java/edu/msg/ro/service/UserManagementBean.java
+++ jbugs-business/src/main/java/edu/msg/ro/service/UserManagementBean.java
@@ -3,7 +3,7 @@ package edu.msg.ro.service;
 import edu.msg.ro.boundary.UserManagement;
 import edu.msg.ro.exceptions.BusinessException;
 import edu.msg.ro.exceptions.ExceptionCode;
-import edu.msg.ro.persistence.user.dao.UserPersistanceManagement;
+import edu.msg.ro.persistence.user.dao.UserPersistenceManagement;
 import edu.msg.ro.persistence.user.entity.User;
 import edu.msg.ro.transfer.UserDTO;
 import edu.msg.ro.transfer.UserDTOHelper;
@@ -14,8 +14,12 @@ import org.apache.logging.log4j.Logger;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.validation.constraints.NotNull;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 @Stateless
 public class UserManagementBean implements UserManagement {
@@ -23,21 +27,27 @@ public class UserManagementBean implements UserManagement {
 
     private static final int MAX_LAST_NAME_LENGTH = 5;
     private static final int MIN_USERNAME_LENGTH = 6;
-    private static Logger logger = LogManager.getLogger(UserManagementBean.class);
+    private static final Logger logger = LogManager.getLogger(UserManagementBean.class);
 
     @EJB
-    private UserPersistanceManagement userPersistanceManagement;
+    private UserPersistenceManagement userPersistenceManagement;
 
     @Override
     public UserDTO createUser(UserDTO userDTO) throws BusinessException {
         logger.log(Level.INFO, "Entered createUser method");
         //TODO trim firstName and lastName
 
-        if (!isUserValidForCreation(userDTO) || isValidEmail(userDTO.getEmail())){
+        if (!isUserValidForCreation(userDTO) || !isValidEmail(userDTO.getEmail())) {
             throw new BusinessException(ExceptionCode.USER_VALIDATION_EXCEPTION);
         }
+
+        if (!userPersistenceManagement.getByEmail(userDTO.getEmail()).isEmpty())
+            throw new BusinessException(ExceptionCode.USER_VALIDATION_EXCEPTION);
+
         User user = UserDTOHelper.toEntity(userDTO);
-        userPersistanceManagement.addUser(user);
+        user.setStatus(true);
+        user.setUsername(generateUsername(user.getFirstName(), user.getLastName()));
+        userPersistenceManagement.addUser(user);
         return UserDTOHelper.fromEntity(user);
     }
 
@@ -45,30 +55,43 @@ public class UserManagementBean implements UserManagement {
     public String generateUsername(@NotNull final String firstName, @NotNull final String lastName) {
         StringBuilder username = new StringBuilder();
         if (lastName.length() >= MAX_LAST_NAME_LENGTH) {
-            username.append(lastName.substring(0, MAX_LAST_NAME_LENGTH) + firstName.charAt(0));
+            username.append(lastName, 0, MAX_LAST_NAME_LENGTH).append(firstName.charAt(0));
         } else if (lastName.length() + firstName.length() >= MIN_USERNAME_LENGTH) {
-            username.append(lastName + firstName.substring(0, MIN_USERNAME_LENGTH - firstName.length()));
+            username.append(lastName).append(firstName, 0, MIN_USERNAME_LENGTH - firstName.length());
         } else {
-            username.append(lastName + firstName);
+            username.append(lastName).append(firstName);
             int usernameLength = username.length();
             for (int i = 0; i < MIN_USERNAME_LENGTH - usernameLength; i++) {
                 username.append("0");
             }
         }
-        return username.toString().toLowerCase();
+        return username.toString().toLowerCase() + createSuffix(username.toString());
     }
 
-    private boolean isUserValidForCreation(UserDTO userDTO){
+    protected String createSuffix(String username) {
+        List<String> usernameLike = userPersistenceManagement.getUsersNameLikeUsername(username);
+        Optional<Integer> max = usernameLike
+                .stream()
+                .map(s -> Integer.parseInt(s.substring(6, s.length())))
+                .max(Comparator.naturalOrder())
+                .map(x -> x + 1);
+        if (max.isPresent())
+            return max.toString();
+        else
+            return "";
+    }
+
+    private boolean isUserValidForCreation(UserDTO userDTO) {
         return userDTO.getFirstName() != null && userDTO.getLastName() != null &&
                 userDTO.getEmail() != null && userDTO.getPassword() != null &&
                 userDTO.getPhoneNumber() != null;
     }
 
-    private boolean isValidEmail(String email){
+    private boolean isValidEmail(String email) {
         final Pattern VALID_EMAIL_ADDRESS_REGEX =
                 Pattern.compile("^[A-Z0-9._%+-]+@msggroup.com$", Pattern.CASE_INSENSITIVE);
 
-        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(email);
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
         return matcher.find();
     }
 }
--- jbugs-client/src/main/java/servlet/TestServlet.java
+++ jbugs-client/src/main/java/servlet/TestServlet.java
@@ -10,134 +10,60 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.msg.ro.boundary.UserManagement;
+import edu.msg.ro.exceptions.BusinessException;
 import edu.msg.ro.persistence.user.dao.PermissionManagement;
-import edu.msg.ro.persistence.user.dao.UserPersistanceManagement;
+import edu.msg.ro.persistence.user.dao.UserPersistenceManagement;
 import edu.msg.ro.persistence.user.entity.Permission;
 import edu.msg.ro.persistence.user.entity.Role;
+import edu.msg.ro.service.UserManagementBean;
+import edu.msg.ro.transfer.UserDTO;
 
-@WebServlet(urlPatterns = { "/TestServlet" })
+@WebServlet(urlPatterns = {"/TestServlet"})
 public class TestServlet extends HttpServlet {
 
-
-	@EJB
-	private PermissionManagement permissionManagement;
-
-	@EJB
-	private UserPersistanceManagement userPersistanceManagement;
+    @EJB
+    private UserManagement userManagementBean;
 
 
-	/**
-	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-	 * methods.
-	 *
-	 * @param request
-	 *            servlet request
-	 * @param response
-	 *            servlet response
-	 * @throws ServletException
-	 *             if a servlet-specific error occurs
-	 * @throws IOException
-	 *             if an I/O error occurs
-	 */
-	protected void processRequest(final HttpServletRequest request, final HttpServletResponse response)
-			throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req,resp);
+    }
 
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    protected void processRequest(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setEmail("adsf@msggroup.com");
+        userDTO.setFirstName("asdf");
+        userDTO.setLastName("SADF");
+        userDTO.setPassword("asdsdfdfg");
+        userDTO.setPhoneNumber("1234");
+        try {
+            userManagementBean.createUser(userDTO);
+        } catch (BusinessException e) {
+            e.printStackTrace();
+        }
 
-		Role r1 = new Role("type1");
-		Role r2 = new Role("type2");
-		Permission p1 = new Permission("type1","desc1");
-		Permission p2 = new Permission("type2","desc2");
-
-
-		permissionManagement.addPermission(p1);
-
-		response.setContentType("text/html;charset=UTF-8");
-		try (PrintWriter out = response.getWriter()) {
-			out.println("<!DOCTYPE html>");
-			out.println("<html>");
-			out.println("<head>");
-			out.println("<title>Test EJB Bean New</title>");
-			out.println("</head>");
-			out.println("<body>");
-			out.println(userPersistanceManagement.getAllUsers());
-			out.println("</body>");
-			out.println("</html>");
-		}
-
-
-		//permissionManagement.addPermission(p1);
-		//permissionManagement.addPermission(p2);
-
-
-		//permissionManagement.addPermission(p1);
-		//permissionManagement.createPermissionForRole(r1,p1);
-	}
-
-	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on
-	// the + sign on the left to edit the code.">
-	/**
-	 * Handles the HTTP <code>GET</code> method.
-	 *
-	 * @param request
-	 *            servlet request
-	 * @param response
-	 *            servlet response
-	 * @throws ServletException
-	 *             if a servlet-specific error occurs
-	 * @throws IOException
-	 *             if an I/O error occurs
-	 */
-	@Override
-	protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
-			throws ServletException, IOException {
-		processRequest(request, response);
-	}
-
-	/**
-	 * Handles the HTTP <code>POST</code> method.
-	 *
-	 * @param request
-	 *            servlet request
-	 * @param response
-	 *            servlet response
-	 * @throws ServletException
-	 *             if a servlet-specific error occurs
-	 * @throws IOException
-	 *             if an I/O error occurs
-	 */
-	@Override
-	protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
-			throws ServletException, IOException {
-		processRequest(request, response);
-	}
-
-	/**
-	 * Returns a short description of the servlet.
-	 *
-	 * @return a String containing servlet description
-	 */
-	@Override
-	public String getServletInfo() {
-		return "Short description";
-	}// </editor-fold>
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Test EJB Bean New</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
 }
-
-
-/*
-@Singleton
-@Startup
-class TestingClass{
-
-	@PostConstruct
-	public void printBeforeTime(){
-		System.out.println("BEFORE:: " + java.time.LocalTime.now());
-	}
-
-	@PreDestroy
-	public void printAfterTime(){
-		System.out.println("AFTER:: " + java.time.LocalTime.now());
-	}
-
-
-}
-*/
